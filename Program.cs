f313f0f1763300fd9cfa97193e064ebe440f5e27
sanchezadept <shokker745@gmail.com>
2022-02-07 14:30:23 +0200

    domashka

--- Program.cs
+++ Program.cs
@@ -10,31 +10,48 @@ namespace _2lesson
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What your name?");
-            string name = Console.ReadLine();
-            Console.WriteLine("What name second person?");
-            string name2 = Console.ReadLine();
-            if (name=="Artjom" && name2 == "Timofei" || name=="Timofei" && name2=="Artjom")
+            //Console.WriteLine("What your name?");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("What name second person?");
+            //string name2 = Console.ReadLine();
+            //if (name=="Artjom" && name2 == "Timofei" || name=="Timofei" && name2=="Artjom")
+            //{
+            //    Console.WriteLine($"{name} and {name2} sit together");
+            //}
+            //else if (name == "German" && name2 == "Kevin" || name=="Kevin" && name2 == "German")
+            //{
+            //    Console.WriteLine($"{name} and {name2} sit together");
+            //}
+            //else if (name == "Ilya" && name2 == "Anton" || name == "Anton" && name2 == "Ilya")
+            //{
+            //    Console.WriteLine($"{name} and {name2} sit together");
+            //}
+            //else if (name == "Denis" && name2 == "Artjom" || name == "Artjom" && name2 == "Denis")
+            //{
+            //    Console.WriteLine($"{name} and {name2} sit together");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Neighbors is out");
+            //}
+            int plosad = 0;
+            Console.WriteLine("Какая длина стены a?");
+            int dlina1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Какая длина стены b?");
+            int dlina2 = int.Parse(Console.ReadLine());
+            plosad = dlina1 * dlina2;
+            Console.WriteLine($"Площадь пола = {plosad}");
+            Console.WriteLine("Хотели бы вы сделать ремонт?");
+            string remont = Console.ReadLine();
+            if (remont.ToUpper() == "YES" || remont.ToUpper() == "ДА")
             {
-                Console.WriteLine($"{name} and {name2} sit together");
-            }
-            else if (name == "German" && name2 == "Kevin" || name=="Kevin" && name2 == "German")
-            {
-                Console.WriteLine($"{name} and {name2} sit together");
-            }
-            else if (name == "Ilya" && name2 == "Anton" || name == "Anton" && name2 == "Ilya")
-            {
-                Console.WriteLine($"{name} and {name2} sit together");
-            }
-            else if (name == "Denis" && name2 == "Artjom" || name == "Artjom" && name2 == "Denis")
-            {
-                Console.WriteLine($"{name} and {name2} sit together");
+                Console.WriteLine("Сколько стоит квадратный метр?");
+                int odinmetr = int.Parse(Console.ReadLine());
             }
             else
             {
-                Console.WriteLine("Neighbors is out");
+                Console.WriteLine("Ну тогда до связи!");
             }
-
 
 
 
